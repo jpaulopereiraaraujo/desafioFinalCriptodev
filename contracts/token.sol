3a4ac5ff285af157a847cee6705e057a9035f049
Joao <79815659+jpaulopereiraaraujo@users.noreply.github.com>
2022-06-14 19:51:44 -0300

    Add files via upload

---
+++ contracts/token.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./libs.sol";
+
+interface IERC20 {
+    //Implementado (mais ou menos)
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    //Não implementados (ainda)
+    //function allowence(address owner, address spender) external view returns(uint256);
+    //function approve(address spender, uint256 amount) external returns(bool);
+    //function transferFrom(address sender, address recipient, uint256 amount) external returns(bool);
+
+    //Implementado
+    event Transfer(address from, address to, uint256 value);
+
+    //Não está implementado (ainda)
+    //event Approval(address owner, address spender, uint256 value);
+}
+
+contract CryptoToken is IERC20 {
+    //Libs
+    using Math for uint256;
+
+    //Enums
+
+    enum Status {
+        ACTIVE,
+        PAUSED,
+        CANCELLED,
+        KILLED
+    }
+
+    //Properties
+    string public constant name = "CryptoToken";
+    string public constant symbol = "CRY";
+    uint8 public constant decimals = 3; //Default dos exemplos é sempre 18
+    uint256 private totalsupply;
+    uint256 private burnable;
+    address private owner;
+    address[] private tokenOwners;
+    address private furnace;
+    Status contractState;
+
+    mapping(address => uint256) private addressToBalance;
+
+    modifier isOwner() {
+        require(address(msg.sender) == owner, "Sender is not owner!");
+        _;
+    }
+
+    // Events
+
+    //Constructor
+    constructor() {
+        uint256 total = 100;
+        totalsupply = total;
+        owner = msg.sender;
+        addressToBalance[owner] = totalsupply;
+        tokenOwners.push(owner);
+        contractState = Status.ACTIVE;
+    }
+
+    //Public Functions
+    function totalSupply() public view override returns (uint256) {
+        require(
+            contractState == Status.ACTIVE,
+            "The Contract is not available now :("
+        );
+        return totalsupply;
+    }
+
+    function balanceOf(address tokenOwner)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        require(
+            contractState == Status.ACTIVE,
+            "The Contract is not available now :("
+        );
+        return addressToBalance[tokenOwner];
+    }
+
+    function burn(uint256 value) public isOwner returns (bool) {
+        //require(contractState == Status.ACTIVE,"The Airdrop is not available now :(");
+        require(
+            contractState == Status.ACTIVE,
+            "The Contract is not available now :("
+        );
+        furnace = 0xf000000000000000000000000000000000000000;
+
+        for (uint256 i = 0; i < tokenOwners.length; i++) {
+            addressToBalance[tokenOwners[i]] = addressToBalance[tokenOwners[i]]
+                .percent(value);
+
+            emit Transfer(
+                tokenOwners[i],
+                furnace,
+                addressToBalance[tokenOwners[i]]
+            );
+        }
+        totalsupply = totalsupply.percent(value);
+
+        return true;
+    }
+
+    function autoBurn(uint256 value) public isOwner {
+        require(
+            contractState == Status.ACTIVE,
+            "The Contract is not available now :("
+        );
+        burnable = value;
+        burn(burnable);
+    }
+
+    function transfer(address receiver, uint256 quantity)
+        public
+        override
+        isOwner
+        returns (bool)
+    {
+        require(
+            contractState == Status.ACTIVE,
+            "The Contract is not available now :("
+        );
+        require(
+            quantity <= addressToBalance[owner],
+            "Insufficient Balance to Transfer"
+        );
+        addressToBalance[owner] = addressToBalance[owner] - quantity;
+        addressToBalance[receiver] = addressToBalance[receiver] + quantity;
+        tokenOwners.push(receiver);
+        autoBurn(burnable);
+
+        emit Transfer(owner, receiver, quantity);
+        return true;
+    }
+
+    //Mint: Adicionar tokens ao total supply
+    function mintToken() public isOwner {
+        require(
+            contractState == Status.ACTIVE,
+            "The Contract is not available now :("
+        );
+        uint256 amount = 50;
+        if (balanceOf(owner) < 51) {
+            totalsupply += amount;
+            addressToBalance[owner] += amount;
+            emit Transfer(owner, owner, 50);
+        }
+    }
+
+    function state() public view returns (Status) {
+        return contractState;
+    }
+
+    function cancelContract() public isOwner {
+        contractState = Status.CANCELLED;
+    }
+
+    function pauseContract() public isOwner {
+        contractState = Status.PAUSED;
+    }
+
+    function activeContract() public isOwner {
+        contractState = Status.ACTIVE;
+    }
+
+    function kill() public isOwner {
+        require(contractState == Status.CANCELLED, "The contract is active");
+        contractState = Status.KILLED;
+        selfdestruct(payable(owner));
+    }
+}
---
+++ contracts/vending.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.7.0 <0.9.0;
+
+contract VendingMachine {
+
+    // Properties
+    address public owner;
+    mapping (address => uint) public GamaToBalances;
+    mapping (address => uint) private EthToBalances;
+    uint256 private gamaBuyValue;
+    uint256 private gamaSellValue;
+
+    // Modifers
+    modifier isOwner(){
+        require(msg.sender == owner, "Only the onwer can make this");
+        _;
+    }
+
+    //Constructor
+
+    constructor(){
+        owner = msg.sender;
+        gamaSellValue = 1;
+        gamaBuyValue = 1;
+    }
+
+    // Public Function
+
+		//Função para Compra de Gama por ETH na máquina de venda.
+    function purchaseGama(uint256 amount) public payable {
+				//um "if" pra indicar o valor mínimo de compra no caso é de 1 eth, já que no exemplo 1 eth = 1gama
+        require(msg.value >= amount * 1 ether, "You must buy at least 1 ether per Gama.");
+				//um "if" pra saber se existe gama suficiente pra vender na máquina da venda.
+        require(GamaToBalances[address(this)] >= amount, "Not enough Gamas in stock to fulfill purchase request.");
+				//decremento do valor comprado em gama da máqunia
+        GamaToBalances[address(this)] -= amount;
+				//incremento do valor comprado para carteira do comprador
+        GamaToBalances[msg.sender] += amount;
+				//incremento do eth pago na transação para o saldo de ETH da máquina de venda
+        EthToBalances[address(this)] += msg.value;
+    }
+
+	    function sellingGama(uint256 amount) public payable {
+			//Aqui são os mesmo comentários do de compra só que pro de venda.
+        //require(GamaToBalances[msg.sender] >= amount * 1 ether, "You must Sell at least 1 ether in Gama.");
+        require(GamaToBalances[msg.sender] >= amount, "Not enough Gamas to trade for ether.");
+        GamaToBalances[msg.sender] -= amount;
+        GamaToBalances[address(this)] += amount;
+				//essas duas ultimas linhas estão diferentes da função de cima por causa de 
+        //um detalhe que percebi, se agente coloca apenas 
+			  //"EthToBalances[address(this)] -= amount" eu estaria adicionado o valor em wei
+        //então quando a carteira vendesse 1 gama = 1 eth a gente receberia 1 wei, 
+				//por isso criei uma var weiToEther com o valor de 1 eth em wei ai 
+				// só multiplicar o amount pela weiToEther que temos o valor em ETHER, 
+        //deu pra sacar? 
+        EthToBalances[address(this)] -= weiToEther(amount);
+        payable(msg.sender).transfer(weiToEther(amount)*gamaSellValue);
+    }
+
+    //Function weiToEther *Converte de wei para ether
+    function weiToEther(uint256 amount) public pure returns(uint256 weiEther){
+        weiEther = amount*1000000000000000000;
+        return weiEther;
+
+    }
+
+    //Functions Getters and Setters
+
+    function getVendingMachineBalanceGama() public view returns(uint256 GamaToken){
+        return GamaToBalances[address(this)];
+
+    }
+
+    function getVendingMachingBalanceEth() public view returns(uint256 EthToken){
+        return EthToBalances[address(this)];
+    }
+
+    function setGamaSellValue(uint256 newValue) public isOwner{
+        require(newValue > 0,"New value must be higher than 0.");
+        gamaSellValue = newValue;
+
+    }
+
+    function setGamaBuyValue(uint256 newValue) public isOwner{
+        require(newValue > 0,"New value must be higher than 0.");
+        gamaBuyValue = newValue;
+    }
+
+    //Funções de manutenção
+
+    function restockGama(uint256 amountGama) public isOwner{
+        require(amountGama > 0,"You can't restock 0 or less Gamas.");
+        GamaToBalances[address(this)] += amountGama;
+    }
+
+    function restockEth() public payable isOwner{
+        require(msg.value >0,"You can't restock 0 or less Ethers.");
+        
+    }
+
+    function contractBalance () public view returns(uint256) {
+      return address(this).balance;
+    }
+
+    function ownerBalance() public view returns(uint256){
+      return address(owner).balance;
+    }
+
+    function toWithdraw(uint256 amountWithdraw) public payable isOwner{
+        require(amountWithdraw <= address(this).balance, "Not enough eth to withdraw.");
+        payable(msg.sender).transfer(weiToEther(amountWithdraw));
+    }
+
+    // Private Functions
+    
+
+    
+}
